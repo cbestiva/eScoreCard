167884f660e291ca0031c8fc9aba937141b02bcf
cbestiva <cbestiva@gmail.com>
2017-04-05 15:02:00 -0700

    adds Home route, controller, view, and component

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,5 @@
+class HomeController < ApplicationController
+  def index
+    # @home_props = { name: "Stranger" }
+  end
+end
---
+++ client/app/bundles/HelloWorld/components/Home.jsx
@@ -0,0 +1,11 @@
+import React, { PropTypes } from 'react';
+
+export default class Home extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>HOME PAGE</h1>
+      </div>
+    );
+  }
+}
